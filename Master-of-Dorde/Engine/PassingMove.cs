2ade5f7368681764ebb8f3dab4340a55970afbd4
devDI <Developer.Dalani.Ioan@yandex.ru>
2019-04-21 21:59:52 +0300

    Change one cymbol (debug a mistake)

--- Master-of-Dorde/Engine/PassingMove.cs
+++ Master-of-Dorde/Engine/PassingMove.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Master_of_Dorde
+using Master_of_Dorde;
 
 namespace Master_of_Dorde.Engine
 {
